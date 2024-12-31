431b96e0d8b382f39771d2faf9bf71aca78bda85
fakhirula <fakhirula27@gmail.com>
2024-12-31 09:23:09 +0700

    auth

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     */
+
+     
+}
