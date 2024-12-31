b52aa235b8b2fee39f8ac7d7aba5ceaf95e83e9c
fakhirula <fakhirula27@gmail.com>
2024-12-31 09:27:56 +0700

    auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     /**
      * Register a new user.
      */
+    public function register(Request $request) {
+        // Setup validator
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
 
-     
+        // Check validator
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Create user
+
+    }
 }
