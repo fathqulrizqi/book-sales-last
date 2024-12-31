33ec3385fe3419342387a494114d3cef94af4124
fakhirula <fakhirula27@gmail.com>
2024-12-31 13:29:30 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api'
 Route::middleware(['auth:api'])->group(function () {
     Route::get('/user', fn(Request $request) => $request->user());
 
-    Route::middleware(['role:admin', 'role:staff'])->group(function () {
+    Route::middleware(['role:admin,staff'])->group(function () {
         Route::apiResource('/books', BookController::class)->only(['store', 'update', 'destroy']);
         Route::apiResource('/genres', GenreController::class)->only(['store', 'update', 'destroy']);
         Route::apiResource('/authors', AuthorController::class)->only(['store', 'update', 'destroy']);
